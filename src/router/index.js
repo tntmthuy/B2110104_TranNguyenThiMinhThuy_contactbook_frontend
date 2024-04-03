ae6235ce099acdd7c865705f6921a77116975a0a
Minh Thuy <trannguyenminhthuyag@gmail.com>
2024-04-04 00:44:40 +0700

    Tao trang them moi mot lien he

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,20 @@ const routes = [
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
+
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
+
+    {
+		path: '/contacts/add',
+		name: 'contact.add',
+		component: () => import('@/views/ContactAdd.vue'),
+		// props: true // truyền các biến trong $route.params vào làm props
+	},
 ];
 const router = createRouter ({
     history: createWebHistory(import.meta.env.BASE_URL),
